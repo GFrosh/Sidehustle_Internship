367eee422a14ef6126f3eef0551ca32565e55326
Gideon Onyegbula <110903404+GFrosh@users.noreply.github.com>
2022-08-26 01:01:04 +0100

    Initial commit

---
+++ INDEX.js
@@ -0,0 +1,11 @@
+//selecting html elements
+let emailElement = document.querySelector("#email")
+console.log(emailElement)
+console.log("hello world")
+let submitButton = document.querySelector("#submit_button")
+submitButton.addEventListener("Click", function )
+
+
+//attaching click listener
+//getting user entered values
+//javascript  validations
