ff63f37865e3401d6a19585ad653aca24285cc9e
Gideon Onyegbula <110903404+GFrosh@users.noreply.github.com>
2022-08-30 15:14:11 +0100

    Initial commit

--- INDEX.js
+++ INDEX.js
@@ -2,3 +2,14 @@
 //attaching click listener
 //getting user entered values
 //javascript  validations
+
+var sh= 'Side Hustle';
+function alertFunction(params) {
+	var hello= 'hello world'
+	alert(hello)
+	// body...
+}
+alertFunction()
+var add = 100;
+document.write(add+add)
+document.write(add-add)
